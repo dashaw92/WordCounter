2bd7e0c919f2d006e7ac3a921ec10869a1c03829
Daniel Shaw <dashaw92@gmail.com>
2017-02-13 13:46:12 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,20 @@
+### Word counter
+
+This is a program that hooks the keyboard at a low level thanks [this library](http://www.dylansweb.com/2014/10/low-level-global-keyboard-hook-sink-in-c-net/).
+
+If a letter key is pressed, it is appended to a string `current_word`. When the space or enter keys are pressed, the `current_word` is checked against all subscribed words, and if it matches any words, that word's counter is incremented by one. The `current_word` value is reset to `""`, ready to parse the next word.
+
+Keyboard shorts
+
+| Combination   | Function                           |
+|:-------------:|:-----------------------------------|
+| F2 + F3       | Show statistics for words          |
+| F2 + F4       | Show statistics for words and exit |
+| F2 + F5       | Add a subscription                 |
+
+By default, the word "like" is tracked, as that was my original intent: To see how many times I typed the word "like" every day.
+
+---
+
+##### Technical
+This program has a small window that is pushed off the desktop. If the program didn't have this window, keyboard hooking wouldn't work, and I couldn't hide the form without the program terminating. Don't ask me, I have no clue why Windows behaves like that.
---
+++ WordCounter/MainClass.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace WordCounter
+{
+	class MainClass
+	{
+		private static string current_word = "";
+		private static LowLevelKeyboardListener listener;
+		private static Key last_key;
+		private static Dictionary<string, int> subscribed;
+		private static Form form;
+
+		static void Main()
+		{
+			subscribed = new Dictionary<string, int>();
+			subscribed.Add("like", 0);
+			listener = new LowLevelKeyboardListener();
+			listener.OnKeyPressed += OnKeyPress;
+			listener.HookKeyboard();
+			form = new Form();
+			form.Width = 0;
+			form.Height = 0;
+			form.ShowInTaskbar = false;
+			form.Shown += Form_Shown;
+			form.Activated += Form_Activated;
+			form.ShowDialog();
+		}
+
+		private static void Form_Activated(object sender, EventArgs e)
+		{
+			form.SetDesktopLocation(-100, -100);
+		}
+
+		private static void Form_Shown(object sender, EventArgs e)
+		{
+			form.SetDesktopLocation(-100, -100);
+		}
+
+		private static void compile_stats()
+		{
+			string output = string.Format("{0,6} {1,15}\n", "Word", "Count");
+			foreach(string item in subscribed.Keys)
+			{
+				output += string.Format("{0,6} {1,15}\n", item, subscribed[item]);
+			}
+			MessageBox.Show(output, "Word counter statistics", MessageBoxButtons.OK);
+		}
+
+		private static void OnKeyPress(object sender, KeyPressedArgs e)
+		{
+			switch (e.KeyPressed.ToString().Length)
+			{
+				case 1:
+					//All options in the Key enum that are one character long are letters.
+					current_word += e.KeyPressed.ToString().ToLower();
+					break;
+				case 2:
+					//F1 -> F9
+					goto case 3;
+				case 3:
+					//F10 -> ...
+					switch(last_key)
+					{
+						case Key.F2:
+							if(e.KeyPressed == Key.F3)
+							{
+								//F2 + F3 pressed (stats)
+								compile_stats();
+								last_key = Key.Enter;
+							}
+							else if(e.KeyPressed == Key.F4)
+							{
+								//F2 + F4 pressed (exits)
+								compile_stats();
+								Application.Exit();
+							} else if(e.KeyPressed == Key.F5)
+							{
+								//F2 + F5 pressed (add subscription)
+								string word = Microsoft.VisualBasic.Interaction.InputBox("Add word subscription", "Enter a word to subscribe to", "", 0, 0).Trim().ToLower();
+								if (!subscribed.ContainsKey(word))
+								{
+									subscribed.Add(word, 0);
+								}
+							}
+							break;
+						case Key.F3:
+							if (e.KeyPressed == Key.F2)
+							{
+								//F2 + F3 pressed (stats)
+								compile_stats();
+								last_key = Key.Enter;
+							}
+							break;
+						case Key.F4:
+							if (e.KeyPressed == Key.F2)
+							{
+								//F2 + F4 pressed (exits)
+								compile_stats();
+								listener.UnHookKeyboard();
+								Application.Exit();
+							}
+							break;
+						case Key.F5:
+							if(e.KeyPressed == Key.F2)
+							{
+								//F1 + F4 pressed (add subscription)
+								string word = Microsoft.VisualBasic.Interaction.InputBox("Add word subscription", "Enter a word to subscribe to", "", 0, 0).Trim().ToLower();
+								if (!subscribed.ContainsKey(word))
+								{
+									subscribed.Add(word, 0);
+								}
+							}
+							break;
+					}
+					last_key = e.KeyPressed;
+					break;
+				case 5:
+					goto case 6;
+				case 6:
+					//Space or Enter
+					if(e.KeyPressed == Key.Space || e.KeyPressed == Key.Enter)
+					{
+						foreach(string item in subscribed.Keys)
+						{
+							if (current_word.Equals(item))
+							{
+								subscribed[item] += 1;
+								break;
+							}
+						}
+						Console.WriteLine("You typed the word \"" + current_word + "\"");
+						current_word = "";
+					}
+					break;
+				default:
+					last_key = Key.Enter;
+					break;
+			}
+		}
+	}
+}
